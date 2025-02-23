ab676cb099db037e551ea62ccf29a358604a1257
Mikhail <mikhail.fedorov.044@mail.ru>
2025-02-23 11:12:28 +0300

    Added get models methods

--- resources/controllers/llm/llm_controller.py
+++ resources/controllers/llm/llm_controller.py
@@ -1,4 +1,4 @@
-"""Класс контроллер отвечающий за работу с языковыми моделями посредствам подключения к олламе
+"""Класс контроллер отвечающий за работу с языковыми моделями посредством подключения к олламе
     TODO:
         Реализовать логику работы с чатами в формате получения сообщений в виде:
             {
@@ -9,14 +9,17 @@
                 }
             }
 """
+from typing import Dict, List
 
-import logging
+from fastapi import HTTPException
 from langchain_ollama import ChatOllama
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_ollama import OllamaEmbeddings
-from resources.helpers.environment_helper import EnvironmentHelper
+from starlette.responses import JSONResponse
 
-logger = logging.basicConfig(level=logging.INFO)
+from resources.helpers.environment_helper import EnvironmentHelper
+import requests
+
 class LLMController:
     def __init__(self):
         self.env = EnvironmentHelper()
@@ -26,7 +29,7 @@ class LLMController:
         self.llm_embenndings = OllamaEmbeddings(model=self.env.ollama_embedding_model, base_url= self.ollama_url)
     
     def get_query_prompt(self) -> PromptTemplate:
-        """Геттер для промпта для поиска в векторной базе данных при использовании ее как объекта."""
+        """Геттер для получения промпта при поиске в векторной базе данных."""
         return PromptTemplate(
             input_variables=["question"],
             template="""Вы - ассистент языковой модели искусственного интеллекта.
@@ -43,4 +46,64 @@ class LLMController:
         Вопрос: {question}
         """
         return ChatPromptTemplate.from_template(template) 
-    
+
+    def get_models(self) -> List:
+        """
+        Поиск моделей для генерации ответа
+
+        :return: Список моделей для генерации ответа
+        """
+        url = self.env.ollama_url + '/api/tags'  # URL для запроса
+        try:
+            response = requests.get(url)  # Выполняем GET-запрос
+            response.raise_for_status()  # Проверяем на наличие ошибок
+        except requests.exceptions.RequestException as e:
+            raise HTTPException(status_code=404, detail=f"Ошибка при выполнении запроса: {e}")
+
+        # Отбор названий моделей
+        models = response.json()
+        model_names = []
+        for model in models['models']:
+            if model['name'] != 'nomic-embed-text:latest':
+                model_names.append({"name": model['name']})
+        # Проверка наличия моделей
+        if not model_names:
+            raise HTTPException(status_code=404, detail="Модели не загружены")
+        return model_names
+
+    def get_embedding_models(self):
+        """
+        Поиск моделей для генерации embeddings
+
+        :return: Список моделей для генерации embeddings
+        """
+        url = self.env.ollama_url + '/api/tags'  # URL для запроса
+        try:
+            response = requests.get(url)  # Выполняем GET-запрос
+            response.raise_for_status()  # Проверяем на наличие ошибок
+        except requests.exceptions.RequestException as e:
+            raise HTTPException(status_code=404, detail=f"Ошибка при выполнении запроса: {e}")
+
+        # Список существующих моделей
+        existing_embedding_models = [
+            "mxbai-embed-large",
+            "nomic-embed-text",
+            "all-minilm"
+        ]
+
+        # Отбор существующих моделей для генерации
+        models = response.json()
+        embedding_models = []
+        # Проверяем каждую полученную модель
+        for model in models['models']:
+            # Проверяем, начинается ли имя модели с любого из ожидаемых
+            for existing_model in existing_embedding_models:
+                if model['name'].startswith(existing_model):
+                    embedding_models.append({"name": model['name']})
+                    break
+
+        # Проверка наличия моделей
+        if not embedding_models:
+            raise HTTPException(status_code=404, detail="Модели для генерации embeddings не найдены")
+
+        return embedding_models
